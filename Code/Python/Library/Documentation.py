8c60f95e92229393619b26b58181aec4d87e8ed3
Ezy Wezy <ezra12325@outlook.com>
2025-08-16 21:40:37 -0400

    Create Documentation.py

---
+++ Code/Python/Library/Documentation.py
@@ -0,0 +1,29 @@
+# These skip the intro/introduction in terminal
+import os
+os.environ["SKIP_INTRO"] = "1"
+
+# This is the importer you can also import certain classes so the entire library doesn't need to load.
+from Library import glib
+#from Library.glib import Extras # <== This is the Extras class
+
+# if you're just importing the entire library you can name them like this.
+music = glib.Music()
+extras = glib.Extras()
+
+# These are the arguments you can change for the entirty of the code they can all be handled hear and can be updated as you please.
+glib.update_arguments({
+    'music': {
+      'volume': 0.5
+    },
+    'debug': False,
+    'intro': False # Do not use this unless you really want to it does infact do nothing and is a placeholder for the library itself
+})
+
+extras.wait(1) # This waits a certain amount of seconds like. (it does support foats [ex. 0.4)
+
+music.play(r"path to mp3.mp3") # This plays a .mp3 volume is handled about in the arguments
+
+music.removefile(b"path to file.file-extension") # This deleted a file for you, you just have to put in the path
+
+music.download(url = 'url here', format = "mp3", out = "output folder here") # This downloads a mp3 from a website that yt_dlp supportsand puts it into the file you want (Mostly a wrapper)
+music.download(url = 'url here', format = "mp4", out = "output folder here") # This downloads a mp4 from a website that yt_dlp supportsand puts it into the file you want (Mostly a wrapper)
