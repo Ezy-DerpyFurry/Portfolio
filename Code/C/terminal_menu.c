6d9c59d0d7f06566f26bb1487d33f1ceb2260fb9
Ezy Wezy <ezra12325@outlook.com>
2025-09-06 01:06:08 -0400

    Update terminal_menu.c

--- Code/C/terminal_menu.c
+++ Code/C/terminal_menu.c
@@ -2,8 +2,11 @@
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
+
+// Defining a basic clear (not a real clear)
 #define clear() printf("\033[2J\033[H");
 
+// This is just to print the menu so I don't need to have it clutter the main code
 void print_terminal_menu(const char *list[], int list_length, int num, const char *opening, const char *selector) {
     clear();
     printf("%s\n", opening);
@@ -12,19 +15,23 @@ void print_terminal_menu(const char *list[], int list_length, int num, const cha
     }
 }
 
+// This is the main code to actually run the terminal
 const char* terminal_menu(const char *list[], int list_length, const char *opening, const char *selector) {
 
+    // Changes terminal's attributes like canceling input
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
+    // Some variables to start with
     char c;
     int num = 1;
 
     print_terminal_menu(list, list_length, num, opening, selector);
 
+    // Main while loop
     while (read(STDIN_FILENO, &c, 1) == 1) {
         if (c == 'q') {
             tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
@@ -32,12 +39,13 @@ const char* terminal_menu(const char *list[], int list_length, const char *openi
             break;
         }
 
+        // First if and else if is just to move up and down on the list
         if (c == 119 || c == 65) {
             num = (num + list_length - 2) % list_length + 1;
         } else if (c == 115 || c == 66) {
             num = num % list_length + 1;
         } else if (c == 10 || c == 67) {
-            tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+            tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Resets terminal's attributes to normal once done
             return list[num-1];
             break;
         }
@@ -45,9 +53,10 @@ const char* terminal_menu(const char *list[], int list_length, const char *openi
         print_terminal_menu(list, list_length, num, opening, selector);
     }
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // Resets terminal's attributes if the function errors or ends wrong
 }
 
+// Example code you can mess around with it
 int main() {
     const char *list[] = {
         "Apple",
