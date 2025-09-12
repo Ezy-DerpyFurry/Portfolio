f01972acc8db582e6f23fa0b199c8b711f7cc914
Ezy Wezy <ezra12325@outlook.com>
2025-09-12 01:41:32 -0400

    Update twosum.py

--- Code/Python/leetcode/twosum.py
+++ Code/Python/leetcode/twosum.py
@@ -6,3 +6,16 @@ class Solution(object):
                 print(nums.index(nums[i]), nums.index(nums[i]) + 1)
                 return nums.index(nums[i]), nums.index(nums[i]) + 1
         return 0
+
+### I followed a tutorial for this one link -> https://www.youtube.com/watch?v=KLlXCFG5TnA
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
